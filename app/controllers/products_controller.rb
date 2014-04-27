056c2ee149581cc546045b1de215078ec5258ed3
Ken Crocken <kcrocken@gmail.com>
2014-04-26 23:29:46 -0400

    Mailer finished, set :test, complete up to Task I

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -66,7 +66,7 @@ class ProductsController < ApplicationController
     @latest_order = @product.orders.order(:updated_at).last
     if stale?(@latest_order)
       respond_to do |format|
-        format.json {render :json => @product.to_json(:include => :orders)}
+        format.atom
       end
     end
   end
